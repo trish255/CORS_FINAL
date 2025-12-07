351bcc73b79f191386c992dec89d80b42ea56680
Trisha gaurano <trishagaurano9@gmail.com>
2025-12-07 18:28:37 +0800

    err

--- CORS_FINAL/Form1.vb
+++ CORS_FINAL/Form1.vb
@@ -3,4 +3,10 @@
         Me.Hide()
         Form2.Show()
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = FormBorderStyle.Sizable
+        Button1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+    End Sub
+
 End Class
